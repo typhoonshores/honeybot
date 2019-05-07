a94fc7777dd958dc1e8609c20a1fa3c335aad52b
Armand Mousavi <typhoonshores@gmail.com>
2019-05-07 00:01:32 -0700

    Update Maths.py
    Added command for Roman Numeral Converter

--- honeybot/plugins/maths.py
+++ honeybot/plugins/maths.py
@@ -19,6 +19,9 @@ returns cosine of number
 >>> .tan <number>
 returns tangent of number
 
+>>> .roman <number>
+returns roman numeral conversion of number
+
 >>> .rand <number1> <number2>
 returns number between number1 and number2
 """
@@ -55,6 +58,30 @@ class Plugin:
                             methods['send'](info['address'], '{}'.format(tangent))
                         except ValueError:
                             methods['send'](info['address'], ".tan must have numbers")
+                    elif msgs[0] == '.roman':
+                        try:
+                            val = [
+                                1000, 900, 500, 400,
+                                100, 90, 50, 40,
+                                10, 9, 5, 4,
+                                1
+                                ]
+                            syb = [
+                                "M", "CM", "D", "CD",
+                                "C", "XC", "L", "XL",
+                                "X", "IX", "V", "IV",
+                                "I"
+                                ]
+                            roman_num = ''
+                            i = 0
+                            while  num > 0:
+                                for _ in range(num // val[i]):
+                                    roman_num += syb[i]
+                                    num -= val[i]
+                                i += 1
+                            methods['send'](info['address'], '{}'.format(roman_num))
+                        except ValueError:
+                            methods['send'](info['address'], ".roman must have numbers")
                     elif msgs[0] == '.rand':
                         try:
                             if int(msgs[1]) >= int(msgs[2]):
